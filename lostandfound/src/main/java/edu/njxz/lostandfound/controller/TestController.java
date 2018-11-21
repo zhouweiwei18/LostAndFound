f072bf6e4aa1bdf79b59e3b335deb90d73065f33
zhouweiwei18 <15950507893@163.com>
2018-11-21 15:34:41 +0800

    解决数据库连接问题

--- lostandfound/src/main/java/edu/njxz/lostandfound/controller/TestController.java
+++ lostandfound/src/main/java/edu/njxz/lostandfound/controller/TestController.java
@@ -4,31 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.njxz.lostandfound.entity.Comment;
 import edu.njxz.lostandfound.entity.Message;
 import edu.njxz.lostandfound.service.TestService;
 
-@Controller
+@RestController
 public class TestController {
 
 	@Autowired
 	private TestService tess;
 
+	/**
+	 * 测试方法
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping("/test")
-	public String test(Model model) {
-		
+	public List<Message> test(Model model) {
+
 		List<Message> messages = tess.showAllMessage();
-		List<List<Comment>> comments = new ArrayList<List<Comment>>();
-		for(int i = 0; i < messages.size(); i++) {
+		/*List<List<Comment>> comments = new ArrayList<List<Comment>>();
+		for (int i = 0; i < messages.size(); i++) {
 			comments.add(tess.showAllCommentByMessageId(messages.get(i).getMessageId()));
 		}
 		model.addAttribute("messages", messages);
-		model.addAttribute("comments", comments);
-		return "test";
+		model.addAttribute("comments", comments);*/
+		// 返回json数据
+		return messages;
 	}
-	
+
 }
--- lostandfound/src/main/java/edu/njxz/lostandfound/service/impl/TestServiceImpl.java
+++ lostandfound/src/main/java/edu/njxz/lostandfound/service/impl/TestServiceImpl.java
@@ -7,29 +7,29 @@ import org.springframework.stereotype.Service;
 
 import edu.njxz.lostandfound.entity.Comment;
 import edu.njxz.lostandfound.entity.Message;
+import edu.njxz.lostandfound.entity.MessageExample;
+import edu.njxz.lostandfound.entity.UserExample;
 import edu.njxz.lostandfound.mapper.CommentMapper;
 import edu.njxz.lostandfound.mapper.MessageMapper;
 import edu.njxz.lostandfound.service.TestService;
 
 @Service
 public class TestServiceImpl implements TestService {
-	
+
 	@Autowired
 	private CommentMapper comm;
-	
+
 	@Autowired
 	private MessageMapper mesm;
 
 	@Override
 	public List<Message> showAllMessage() {
-		return mesm.showAllMessage();
+		return mesm.selectByExample(new MessageExample());
 	}
 
 	@Override
 	public List<Comment> showAllCommentByMessageId(int messageId) {
 		return comm.showAllCommentByMessageId(messageId);
 	}
-	
-	
 
 }
